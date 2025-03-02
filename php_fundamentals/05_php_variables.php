12ddf17b8a7eb8987a4e3e95283f00357569c384
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-02 16:14:38 +0530

    Added new file

---
+++ php_fundamentals/05_php_variables.php
@@ -0,0 +1,30 @@
+<?php
+    //Declaring variables
+    $x = 5;
+    $y = "john";
+
+    //Output variables
+    $txt = "Hello World !\n";
+    echo "Just say $txt";
+
+    //Sum of two variables
+    $a = 5;
+    $b = 20;
+    echo $a + $b."\n";
+
+    //Get the type of the variable
+    $var = 5;
+    var_dump($var);
+
+    //var_dump()
+    var_dump(5);
+    var_dump(5.1);
+    var_dump("Hello");
+    var_dump(true);
+    var_dump([2,3,5]);
+    var_dump(NULL);
+
+    //Assigning multiple values
+    $var1 = $var2 = $var3 = "Fruit";
+    echo "$var1 $var2 $var3";
+?>
