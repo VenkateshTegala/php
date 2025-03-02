55b6ccb98623cb5c3cf14ee7496617af9486d56c
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-02 22:10:45 +0530

    Added new file

---
+++ php_fundamentals/10_php_math.php
@@ -0,0 +1,12 @@
+<?php
+    echo(pi());
+    echo(min(56,23,45,55))."\n";
+    echo(max(56,23,45,55))."\n";
+    echo(abs(-6.7))."\n";
+    echo(sqrt(64))."\n";
+    echo(round(0.60))."\n";
+    echo(round(0.49))."\n";
+    echo(rand())."\n";
+    echo(rand(10,100))."\n";
+
+?>
