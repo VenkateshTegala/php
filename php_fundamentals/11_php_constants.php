295b1873d3d88868603e7189f6321ceb78b9dada
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-02 22:17:18 +0530

    Added new file

---
+++ php_fundamentals/11_php_constants.php
@@ -0,0 +1,14 @@
+<?php
+    //define(name, value);
+    define("greeting", "Hello World!");
+    echo greeting;
+    echo "\n";
+
+    //php const keyword
+    const mycar = "Volvo";
+    echo mycar;
+    echo "\n";
+    //const arrays
+    define("cars", ["Audi", "Mercedes", "Jaguar"]);
+    echo cars[0];
+?>
