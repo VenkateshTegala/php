f717a9f9486bc0a7a489fed0e4402361784136b4
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-02 16:38:18 +0530

    Added new file

---
+++ php_fundamentals/07_php_datatypes.php
@@ -0,0 +1,18 @@
+<?php
+    $x = 5;
+    var_dump($x);
+    $y = 5.5;
+    var_dump($y);
+    $word = "Hello";
+    var_dump($word);
+    $boolean = true;
+    var_dump($boolean);
+    $myarray = array("Apple", "Mango", "Orange");
+    var_dump($myarray);
+    $mynull = null;
+    var_dump($mynull);
+    //type casting
+    $a = 5;
+    $a = (string) $a;
+    var_dump($a);
+?>
