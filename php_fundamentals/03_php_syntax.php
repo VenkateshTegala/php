7642849d89526f6c2952503d6809516f82daf247
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-02 15:59:56 +0530

    Added new file

---
+++ php_fundamentals/03_php_syntax.php
@@ -0,0 +1,14 @@
+<?php
+    //code here
+
+    //not case sensitive
+    ECHO "Hello World !\n";
+    echo "Hello World !\n";
+    EcHO "Hello World !\n";
+
+    //But variables are case sensitive
+    $marks = 5;
+    $MARKS = 10;
+    echo "Marks equal to $marks\n";
+    echo "Marks equal to $MARKS\n";
+?>
