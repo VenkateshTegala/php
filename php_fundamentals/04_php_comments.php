1c5228207f2ac3b7da301384dc28776a076f6ee9
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-02 16:04:02 +0530

    Added new file

---
+++ php_fundamentals/04_php_comments.php
@@ -0,0 +1,16 @@
+<?php
+    //This is a single line comment
+    #This is also a single line comment
+
+    /*
+    This is a 
+    multikine comment
+    */
+
+    //comment to ignore code
+    //echo "Hello world !";
+
+    #comment in middle of the code
+    $x = 5 /* + 15 */ + 10;
+    echo $x;
+?>
