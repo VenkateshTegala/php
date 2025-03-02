a2b3f0babfd57a5e96151b60ec9b02c9324a61cd
venkatesh tegala <tegalavenkatesh.22.csm@anits.edu.in>
2025-03-02 16:31:46 +0530

    Added new file

---
+++ php_fundamentals/06_php_echo_and_print.php
@@ -0,0 +1,13 @@
+<?php
+    echo "Hello\n";
+    //same as :
+    echo("Hello\n");
+
+    //display variables
+    $capital = "Delhi";
+    echo "$capital is the capital of India\n";
+
+    print "Hello\n";
+    //same as:
+    print("Hello\n");
+?>
